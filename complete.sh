a8f972d095aa3a623175efbca944f54974e45164
Daryl Turner <d.turner@arkadin.com>
2020-11-20 14:54:14 +0000

    dont proceed if fzf is escaped/fails

--- complete.sh
+++ complete.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 selected=$(tmux capture-pane -p -S - | $HOME/.tmux-complete/split-input.pl | sort | uniq | fzf)
-echo -n $selected | tmux load-buffer -b completion -
-tmux paste-buffer -b completion
+if [ $? -eq 0 ]; then
+    echo -n $selected | tmux load-buffer -b completion -
+    tmux paste-buffer -b completion
+fi
